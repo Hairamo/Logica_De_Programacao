ac9230062706552c5e7ffde7a33c6cf45ab0bcc2
adriano nunes <105130822+Hairamo@users.noreply.github.com>
2026-05-20 20:43:39 -0300

    aprendemos html e scripts

--- ex006/script.js
+++ ex006/script.js
@@ -1,7 +1,7 @@
 var hora = 20 /* new Date().getHours(); */ 
-if (hora == 5 || hora <= 11){
+if (hora == 5 && hora <= 11){
     console.log(" Bom dia! ");
-}else if(hora ==12 || hora <= 17){
+}else if(hora ==12 && hora <= 17){
     console.log(" Boa Tarde! ");
 }else{
     console.log("Boa noite");
---
+++ ex008/script.js
@@ -0,0 +1,3 @@
+console.log(`Script no HTML...`)
+window.alert(`Olá! Seja Bem-Vindo!`)
+window.prompt(`Qual o seu nome?: `)
