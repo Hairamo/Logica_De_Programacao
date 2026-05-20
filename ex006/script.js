85692b2a47f8cbf7d48d564ac886375ce150b074
adriano nunes <105130822+Hairamo@users.noreply.github.com>
2026-05-20 20:00:41 -0300

    trabalho dia 20

---
+++ ex006/script.js
@@ -0,0 +1,10 @@
+var hora = 20 /* new Date().getHours(); */ 
+if (hora == 5 || hora <= 11){
+    console.log(" Bom dia! ");
+}else if(hora ==12 || hora <= 17){
+    console.log(" Boa Tarde! ");
+}else{
+    console.log("Boa noite");
+}
+
+console.log(hora)
---
+++ ex007/script.js
@@ -0,0 +1,15 @@
+var numero_1 = 20;
+var numero_2 = 30;
+var numero_3 = 50;
+
+
+if (numero_1 > numero_2 && numero_1 > numero_3) {
+    console.log(`O numero é maior ${numero_1}`)
+} else if (numero_2 > numero_1 && numero_2 > numero_3) {
+    console.log(`O numero é maior ${numero_2}`)
+} else if (numero_3 > numero_1 && numero_3 > numero_2) {
+    console.log(`O numero é maior ${numero_3}`)
+} else {
+    console.log(`Eles são iguais`)
+}
+
