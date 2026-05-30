7d33b593e218ae4569c4b3535de4075899e01544
adriano nunes <105130822+Hairamo@users.noreply.github.com>
2026-05-29 21:50:21 -0300

    usamos contador
    fizemos variaveis dos contador jogando para o html e usamos if e else if

--- ex023-atividade/script.js
+++ ex023-atividade/script.js
@@ -1,16 +1,19 @@
 function verificar(){
-for (let cont = 0; cont <=10; cont++){   /* Ele conta tipo temporizador */
-    console.log(`O contador esta na posição ${cont}`)
+    var numero = Number(document.getElementById(`numero`).value)
+    resultado.innerHTML = `Tabuada de <strong>${numero}</strong><br>`
 
+    for (var cont = 0; cont <=10; cont++){
+        resultado.innerHTML += `${numero} x ${cont} = <strong> ${numero * cont} </strong><br>`
+    }
 
-   
-   
-     
-
-
-} 
-
-
+ /* for (let cont = 0; cont <=10; cont++){   /* Ele conta tipo temporizador */
+  /*   console.log(typeof(`O contador esta na posição ${cont}`))
+let numero =  Number(document.getElementById(`numero`).value)
+let resultado = document.getElementById(`resultado`)
+let multi = numero * cont
+console.log(`${numero} x ${cont} = ${multi}` )  
+ if(numero >= 0){ 
+resultado.innerHTML += `<p> <strong>${numero} x ${cont} = ${multi}</strong><p>`  */
+ 
 
 }
-
---
+++ ex024-atividade/script.js
@@ -0,0 +1,40 @@
+
+/* let inicio = 0
+let fim = 10
+let passo = 2
+
+for(inicio; inicio <= fim; inicio+=passo){
+    console.log(`Contagem: ${inicio}`)
+} */
+function certo() {
+
+    var inicio = Number(document.getElementById(`numero1`).value)
+    var fim = Number(document.getElementById(`numero2`).value)
+    var passo = Number(document.getElementById(`numero3`).value)
+    let resultado = document.getElementById(`resultado`)
+    resultado.innerHTML = ''
+
+    if (passo === 0) {
+        resultado.innerHTML = `O valor do Passo não pode ser 0`
+    } else if (inicio < fim) {
+        for (inicio; inicio <= fim; inicio += passo) {
+            if (inicio + passo > fim) {
+                resultado.innerHTML += `${inicio}  \u{1F3C1}`
+
+            } else {
+                resultado.innerHTML += `${inicio} \u{27A1}`
+            }
+
+        }else {
+            for (inicio; inicio >= fim; inicio -= passo) {
+                if (inicio - passo < fim) {
+                    resultado.innerHTML += `${inicio}  \u{1F3C1}`
+
+                } else {
+                    resultado.innerHTML += `${inicio} \u{27A1}`
+                }
+            }
+        }
+
+    }
+}
