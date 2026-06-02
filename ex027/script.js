dd79775803122cfb13e6d41f6cce09800052fea8
adriano nunes <105130822+Hairamo@users.noreply.github.com>
2026-06-01 21:39:58 -0300

    Listas_html_script

---
+++ ex027/script.js
@@ -0,0 +1,10 @@
+var nomes = []
+var resultado = document.getElementById(`resultado`)
+function cadastrar(){
+    let nome = document.getElementById(`nome`).value
+
+    nomes.push(nome)
+    resultado.innerHTML += `<p>${nome} foi cadastrado no sistema.</p>`
+    resultado.innerHTML += `<p>Essa é a lista de cadastrar ${nome.toString()}</p> `
+
+}
