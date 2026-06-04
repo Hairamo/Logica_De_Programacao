3e30bc059d4308ddae42e8e4a9be7c5c28b578e4
adriano nunes <105130822+Hairamo@users.noreply.github.com>
2026-06-03 21:45:18 -0300

    identificar-maior-menor

---
+++ ex029-atividade/script.js
@@ -0,0 +1,6 @@
+let numeros = [10, 45, 3, 99, 2, 104, 1];
+/* let maior = Math.max(...numeros);
+let menor = Math.min(...numeros);
+console.log("Maior:", maior);
+console.log("Menor:", menor); */
+if (maior >=0)
